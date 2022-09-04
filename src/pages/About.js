4ea5d2afecac34d7bd26074a0ed5ad8c8b9f6306
HosSamNeo <81299330+HosSamNeo@users.noreply.github.com>
2022-09-04 19:24:46 +0200

    .

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,10 +1,11 @@
-import React from 'react'
+import React from 'react';
 
 const About = () => {
   return (
-    <div>
-      <h2>about page</h2>
-    </div>
+    <section className='section about-section'>
+      <h2 className='section-title'>About Us</h2>
+      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fugiat explicabo molestias expedita aliquam illum suscipit nesciunt totam, exercitationem necessitatibus soluta, distinctio, at sint hic. Error incidunt mollitia consequatur, ratione fugit a sit. Molestiae ratione quaerat quis, aspernatur aliquid blanditiis facilis?</p>
+    </section>
   )
 }
 
